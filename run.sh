70168d244b4fd520fb388e28c42d8e33dd125f6e
nickhsine <nickhsine97753017@gmail.com>
2017-04-21 14:51:22 +0000

    [Delete] Delete run.sh.

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# npm run fetch;
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "category $1"
-echo "timerange $2";
-echo "start page $3";
-$DIR/node_modules/.bin/babel-node $DIR/fetch.js $1 $2 $3;
