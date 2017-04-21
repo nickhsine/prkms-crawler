2cf7128f6f0d08090c80cf7833d8a16c58545a4a
nickhsine <nickhsine97753017@gmail.com>
2017-04-21 14:29:58 +0000

    [Update] Update run.sh. Get relative path to run the program

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # npm run fetch;
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "category $1"
 echo "timerange $2";
 echo "start page $3";
-./node_modules/.bin/babel-node ./fetch.js $1 $2 $3;
+$DIR/node_modules/.bin/babel-node $DIR/fetch.js $1 $2 $3;
